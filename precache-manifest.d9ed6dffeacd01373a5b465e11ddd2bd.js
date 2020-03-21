3b810a57c155a544bf62c3e3bf7b8cbedf282eb0
ferdyanggara <moeharyoso89@gmail.com>
2020-03-21 22:08:18 +0700

    Updates

---
+++ precache-manifest.d9ed6dffeacd01373a5b465e11ddd2bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b7cc312926447de504a65e1d6197341",
+    "url": "/reduxexample/index.html"
+  },
+  {
+    "revision": "efbbbe01880b85241856",
+    "url": "/reduxexample/static/css/main.c9b7e7ef.chunk.css"
+  },
+  {
+    "revision": "29f45dd0ff2065f03702",
+    "url": "/reduxexample/static/js/2.01a5bed3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reduxexample/static/js/2.01a5bed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efbbbe01880b85241856",
+    "url": "/reduxexample/static/js/main.eed07314.chunk.js"
+  },
+  {
+    "revision": "2f6c6c29f0b5cdedc915",
+    "url": "/reduxexample/static/js/runtime-main.d7eb6ceb.js"
+  }
+]);
